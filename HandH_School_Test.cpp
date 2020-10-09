7ef3a5eb42b3e2b15eb6cfee073548f0ba438db9
AlexKormak <72575582+AlexKormak@users.noreply.github.com>
2020-10-09 21:05:35 +0300

    Add files via upload

---
+++ HandH_School_Test.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <cstdio>
+using namespace std;
+
+void fillVector(vector<int> &arr)
+{
+    int element;
+
+    cout << "Enter an array of integers: ";
+
+    cin >> element;
+    arr.push_back(element);
+
+    while (getchar() != '\n')
+        {
+            cin >> element;
+            arr.push_back(element);
+        }
+}
+
+int getMaxElementIndex(vector<int> &arr)
+{
+    int maxElement, maxElementIndex;
+
+    maxElement = arr[0];
+    maxElementIndex = 0;
+
+    if (arr.size() > 1)
+    {
+        for (int i = 1; i < arr.size(); ++i)
+            {
+                if (arr[i] > maxElement)
+                {
+                    maxElement = arr[i];
+                    maxElementIndex = i;
+                }
+            }
+    }
+
+    return maxElementIndex;
+}
+
+int main()
+{
+    vector<int> arr;
+
+    fillVector(arr);
+
+    cout << getMaxElementIndex(arr) << "\n";
+
+    return 0;
+}
